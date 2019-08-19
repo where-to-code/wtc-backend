110361aaf0a53eac045bb51b08f9f316052c7f95
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-19 12:50:42 +0100

    Server entry file

---
+++ index.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server started at port ${PORT}`));
