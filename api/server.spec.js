53b2544491dbc1b4f37524eb898601eb63faa24b
basilcea <ogbonna.basil3@gmmail.com>
2019-08-20 19:43:39 +0100

    add coverage icon - remove force exit for flag

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,14 +2,15 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('/ [GET]', () => {
-  it('returns a 200', () => {
+  it('returns a 200', (done) => {
     request(server)
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200);
+      done
   });
 
-  it('returns a message', () =>
+  it('returns a message', (done) =>
     request(server)
       .get('/')
       .expect('Content-Type', /json/)
@@ -18,14 +19,16 @@ describe('/ [GET]', () => {
         expect(res.body.message).toEqual(
           'The Where-to-Code Server is up!',
         );
+        done()
       }));
 
-  it('catches and handles bad request', () =>
+  it('catches and handles bad request', (done) =>
     request(server)
       .get('/very-inappropriate-url')
       .expect('Content-Type', /json/)
       .expect(404)
       .then(res => {
         expect(res.body.message).toEqual('No endpoint matches that URL.');
+        done()
       }));
 });
