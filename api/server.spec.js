d1cdfac3f8f2f9a2432f2b4f4cfaba201378d801
basilcea <ogbonna.basil3@gmmail.com>
2019-08-21 08:14:45 +0100

    add linting to file

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,35 +2,33 @@ const request = require('supertest');
 const server = require('./server');
 
 describe('/ [GET]', () => {
-  it('returns a 200', (done) => {
+  it('returns a 200', done => {
     request(server)
       .get('/')
       .expect('Content-Type', /json/)
-      .then(res =>{
-        expect(res.status).toEqual(200)
-        done()
-      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        done();
+      });
   });
 
-  it('returns a message', (done) =>
+  it('returns a message', done =>
     request(server)
       .get('/')
       .expect('Content-Type', /json/)
       .expect(200)
       .then(res => {
-        expect(res.body.message).toEqual(
-          'The Where-to-Code Server is up!',
-        );
-        done()
+        expect(res.body.message).toEqual('The Where-to-Code Server is up!');
+        done();
       }));
 
-  it('catches and handles bad request', (done) =>
+  it('catches and handles bad request', done =>
     request(server)
       .get('/very-inappropriate-url')
       .expect('Content-Type', /json/)
       .expect(404)
       .then(res => {
         expect(res.body.message).toEqual('No endpoint matches that URL.');
-        done()
+        done();
       }));
 });
