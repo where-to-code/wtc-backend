1be4989603b4eee6a6a145f64bf595037798c775
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-22 18:50:49 +0100

    Refactored the tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -9,33 +9,29 @@ afterAll(async () => {
   await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
 });
 describe('/ [GET]', () => {
-
-  it('returns a 200', () => {
-    return request(server)
+  it('returns a 200', () =>
+    request(server)
       .get('/')
       .expect('Content-Type', /json/)
       .then(res => {
         expect(res.status).toEqual(200);
-      });
-  });
+      }));
 
-  it('returns a message', (done) => {
-    return request(server)
+  it('returns a message', done =>
+    request(server)
       .get('/')
       .expect('Content-Type', /json/)
-      .then((res) => {
+      .then(res => {
         expect(res.status).toEqual(200);
         expect(res.body.message).toEqual('The Where-to-Code Server is up!');
         done();
-      });
-  });
-  it('catches and handles bad request', () => {
-    return request(server)
+      }));
+  it('catches and handles bad request', () =>
+    request(server)
       .get('/very-inappropriate-url')
       .expect('Content-Type', /json/)
-      .then((res) => {
+      .then(res => {
         expect(res.status).toEqual(404);
         expect(res.body.message).toEqual('No endpoint matches that URL.');
-      });
-  });
+      }));
 });
