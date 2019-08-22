08e596dffd63c442e74cda65b29295266893d24e
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-22 17:51:17 +0100

    Handles the get single location req

--- locations/locationController.js
+++ locations/locationController.js
@@ -1,11 +1,12 @@
-const getLocations = require('./locationModel');
+const Model = require('./locationModel');
 const statusHandler = require('../helpers/statusHandler');
 
 // eslint-disable-next-line consistent-return
 const getAllLocationsCloseToUser = async (req, res) => {
   const { lat, long } = req.query;
+
   try {
-    const data = await getLocations(lat, long);
+    const data = await Model.getLocations(lat, long);
 
     if (data.length === 0) {
       return statusHandler(
@@ -14,10 +15,27 @@ const getAllLocationsCloseToUser = async (req, res) => {
         'There is currently no locations around you',
       );
     }
+
     return statusHandler(res, 200, data);
   } catch (error) {
     return statusHandler(res, 500, error.toString());
   }
 };
 
-module.exports = { getAllLocationsCloseToUser };
+const getSingleLocation = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const location = await Model.getSingleLocation(id);
+
+    if (!location) {
+      return statusHandler(res, 404, 'No location matches the id provided');
+    }
+
+    return statusHandler(res, 200, location);
+  } catch (error) {
+    return statusHandler(res, 500, error.toString());
+  }
+};
+
+module.exports = { getAllLocationsCloseToUser, getSingleLocation };
