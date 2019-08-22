e2ca244cc26dd2e6e6b2ef08121fe0a583aaffdc
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-08-22 10:07:39 +0100

    Fix: Jest test timeout and exit command added

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,7 +1,15 @@
 const request = require('supertest');
 const server = require('./server');
 
+// afterEach((done) => {
+//   return server && server.close(done);
+// });
+
+afterAll(async () => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+});
 describe('/ [GET]', () => {
+
   it('returns a 200', () => {
     return request(server)
       .get('/')
@@ -11,13 +19,14 @@ describe('/ [GET]', () => {
       });
   });
 
-  it('returns a message', () => {
+  it('returns a message', (done) => {
     return request(server)
       .get('/')
       .expect('Content-Type', /json/)
       .then((res) => {
         expect(res.status).toEqual(200);
         expect(res.body.message).toEqual('The Where-to-Code Server is up!');
+        done();
       });
   });
   it('catches and handles bad request', () => {
