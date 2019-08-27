34a8cf31955ad33ebee5b5902faf780af144a605
basilcea <ogbonna.basil3@gmmail.com>
2019-08-27 17:11:15 +0100

    create route for user to login

--- users/index.js
+++ users/index.js
@@ -1,1 +1,11 @@
 // export the route from here
+
+const express = require('express');
+const user = require('./userController');
+const validate = require('../middleware/validations');
+
+const router = express.Router();
+
+router.post('/auth/login', validate.validateLogin, user.loginUser);
+
+module.exports = router;
