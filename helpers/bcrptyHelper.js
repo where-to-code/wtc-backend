46ca2ec6fd355971f7ceacfff44bd01254b49e9b
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-08-27 18:15:00 +0100

    create password hasher using Bcrypt module and add JWT for token generation

---
+++ helpers/bcrptyHelper.js
@@ -0,0 +1,10 @@
+const bcrypt = require('bcryptjs');
+
+const hashPassword = (password) => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+};
+
+exports.module = {
+  hashPassword,
+};
