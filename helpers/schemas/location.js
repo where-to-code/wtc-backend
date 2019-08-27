f6214c7b27af197b69330fc0f521e030efee65c0
basilcea <ogbonna.basil3@gmmail.com>
2019-08-27 17:01:58 +0100

    add validations schema for login

--- helpers/schemas/location.js
+++ helpers/schemas/location.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-fallthrough */
+/* eslint-disable no-param-reassign */
 const joi = require('joi');
 
 const lat = joi
@@ -25,7 +27,8 @@ const lat = joi
 const long = joi
   .number()
   .invalid('')
-  .required().error(errors => {
+  .required()
+  .error(errors => {
     errors.forEach(err => {
       // eslint-disable-next-line default-case
       switch (err.type) {
@@ -44,12 +47,79 @@ const long = joi
     return errors;
   });
 
+const queryId = joi
+  .string()
+  .required()
+  .error(errors => {
+    errors.forEach(err => {
+      switch (err.type) {
+        case 'any.required':
+          err.message = 'id is required';
+          break;
+        case 'any.empty':
+          err.message = 'id cannot be empty';
+          break;
+        default:
+          break;
+      }
+    });
+  });
+const email = joi.string().trim().email().invalid('')
+  .required()
+  .error(
+    (errors) => {
+      errors.forEach((err) => {
+        switch (err.type) {
+          case 'any.required':
+            err.message = 'Email field is required';
+            break;
+          case 'any.empty':
+            err.message = 'email cannot be empty';
+            break;
+          case 'string.email':
+            err.message = 'incorrect email format. e.g example@mymail.com';
+            break;
+          default:
+            break;
+        }
+      });
+      return errors;
+    },
+  );
+
+const password = joi.string().trim().invalid('').regex(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/)
+  .required()
+  .error(
+    (errors) => {
+      errors.forEach((err) => {
+        switch (err.type) {
+          case 'any.required':
+            err.message = 'password field is required';
+            break;
+          case 'any.empty':
+            err.message = 'password cannot be empty';
+            break;
+          case 'string.regex.base':
+            err.message = 'Password must be beween 6 and 15 characters and contain letters and numbers ';
+            break;
+          default:
+            break;
+        }
+      });
+      return errors;
+    },
+  );
+const queryLogin = joi.object().keys({
+  password,
+  email,
+});
 const querySchema = joi.object().keys({
   lat,
   long,
 });
 
-
 module.exports = {
   querySchema,
+  queryId,
+  queryLogin,
 };
--- middleware/validations.js
+++ middleware/validations.js
@@ -1,5 +1,8 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-plusplus */
 const joi = require('joi');
 const schema = require('../helpers/schemas/location');
+const statusHandler = require('../helpers/statusHandler');
 
 const validate = (value, scheme, res, next) => {
   joi.validate(
@@ -12,20 +15,20 @@ const validate = (value, scheme, res, next) => {
         for (let i = 0; i < err.details.length; i++) {
           errMsg.push(err.details[i].message);
         }
-        return res.status(400).json({
-          status: 400,
-          error: errMsg,
-        });
-      } else {
-        next();
+        return statusHandler(res, 400, errMsg);
       }
+      next();
     },
   );
 };
 const validateQuery = (req, res, next) => {
-  validate(req.query, schema.querySchema, res, next);
+  return validate(req.query, schema.querySchema, res, next);
+};
+const validateId = (req, res, next) => {
+  return validate(req.params, schema.queryId, res, next);
 };
 
 module.exports = {
   validateQuery,
+  validateId,
 };
