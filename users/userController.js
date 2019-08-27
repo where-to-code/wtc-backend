19387ea03589afc376fecfc05de077065a35a366
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-08-27 18:16:31 +0100

    add logic for user registration in the controller

---
+++ users/userController.js
@@ -0,0 +1,21 @@
+const statusHandler = require('../helpers/statusHandler');
+const bcryptHelper = require('../helpers/bcryptHelper');
+
+const registerNewUser = async (req, res) => {
+  const {
+    firstname, lastname, email, password: pwd,
+  } = req.body;
+  const password = bcryptHelper(pwd);
+  const user = {
+    firstname, lastname, email, password,
+  };
+  try {
+    const newUser = await module.registerUser();
+    return statusHandler(res, 200, newUser);
+  } catch (err) {
+    return statusHandler(res, 500, err.toString());
+  }
+};
+
+
+module.exports = { registerNewUser };
