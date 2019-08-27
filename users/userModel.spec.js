1b3a77f45a7ab6c8b088cf56ce3eb48e0e1874a5
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-08-27 16:40:58 +0100

    add more tests for the model

--- users/userModel.spec.js
+++ users/userModel.spec.js
@@ -12,15 +12,26 @@ describe('Model for users', () => {
   afterEach(() => db.migrate.rollback());
 });
 
-
 describe('/POST auth.register test suite', () => {
+  const user = {
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'johndoe@me.com',
+    password: '12345678',
+  };
+  it('should not have null values', () => {
+    expect(user.firstname).not.toBeNull();
+    expect(user.lastname).not.toBeNull();
+    expect(user.email).not.toBeNull();
+    expect(user.password).not.toBeNull();
+  });
+  it('should not have null undefined', () => {
+    expect(user.firstname).not.toBeUndefined();
+    expect(user.lastname).not.toBeUndefined();
+    expect(user.email).not.toBeUndefined();
+    expect(user.password).not.toBeUndefined();
+  });
   it('should accepts an object as argument with keys and values', async () => {
-    const user = {
-      firstname: 'John',
-      lastname: 'Doe',
-      email: 'johndoe@me.com',
-      password: '12345678',
-    };
     expect(user).toEqual(user);
     expect(user).toHaveProperty('firstname', 'John');
     expect(user).toHaveProperty('lastname', 'Doe');
