6d2dc1a918deb1560f3ad307e1257ebb32465992
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-29 09:54:23 +0100

    Awaited the hash password function;

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,37 +1,37 @@
 const { hashPassword } = require('../../helpers/bcryptHelper');
 
 exports.seed = knex =>
-  knex.schema.raw('TRUNCATE TABLE users, reviews CASCADE').then(() =>
+  knex.schema.raw('TRUNCATE TABLE users, reviews CASCADE').then(async () =>
     knex('users').insert([
       {
         firstname: 'John',
         lastname: 'Doe',
         email: 'jh@john.com',
-        password: hashPassword('12345'),
+        password: await hashPassword('12345'),
       },
       {
         firstname: 'Jane',
         lastname: 'Doe',
         email: 'jn@john.com',
-        password: hashPassword('12345'),
+        password: await hashPassword('12345'),
       },
       {
         firstname: 'Will',
         lastname: 'Smith',
         email: 'fresh@prince.com',
-        password: hashPassword('12345'),
+        password: await hashPassword('12345'),
       },
       {
         firstname: 'Vin',
         lastname: 'Diesel',
         email: 'vin@diesel.com',
-        password: hashPassword('12345'),
+        password: await hashPassword('12345'),
       },
       {
         firstname: 'Van',
         lastname: 'Damme',
         email: 'van@damme.com',
-        password: hashPassword('12345'),
+        password: await hashPassword('12345'),
       },
     ]),
   );
