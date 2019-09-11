0d650b324bbe4556db55e34e72bca19ae4bc81a9
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-09-11 11:54:06 +0100

    remove string and chanfe to text, input lenght

--- database/migrations/20190819175238_locations.js
+++ database/migrations/20190819175238_locations.js
@@ -1,13 +1,13 @@
 exports.up = knex =>
   knex.schema.createTable('locations', table => {
     table.increments();
-    table.string('name', 100).notNullable();
+    table.text('name', 100).notNullable();
     table.text('description').notNullable();
-    table.string('image_url').notNullable();
-    table.string('address', 250).notNullable();
-    table.string('longitude', 100).notNullable();
-    table.string('latitude', 100).notNullable();
-    table.string('place_id', 100);
+    table.text('image_url').notNullable();
+    table.text('address', 250).notNullable();
+    table.string('longitude').notNullable();
+    table.string('latitude').notNullable();
+    table.string('place_id');
     table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
   });
 
