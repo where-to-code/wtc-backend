dee8c98d3e708c388d840f51d02ef085fea4b39a
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-12 11:14:10 +0100

    Spaced out the block of codes

--- reviews/reviewController.js
+++ reviews/reviewController.js
@@ -4,16 +4,19 @@ const statusHandler = require('../helpers/statusHandler');
 
 const addReview = async (req, res) => {
   const { id } = req.params;
+
   const {
     quietness,
     wifi_speed,
     community,
     accessibility,
     description,
-    user_id
+    user_id,
   } = req.body;
+
   try {
     const location = await Location.getSingleLocation(id);
+
     if (location) {
       const [newReview] = await Review.addReview({
         location_id: id,
@@ -22,15 +25,22 @@ const addReview = async (req, res) => {
         community,
         accessibility,
         description,
-        user_id
+        user_id,
       });
+
       return statusHandler(res, 201, newReview);
     }
+
     return statusHandler(res, 404, 'This location does not exist');
   } catch (error) {
     if (error.routine === '_bt_check_unique') {
-      return statusHandler(res, 500, 'You have already reviewed this location.');
+      return statusHandler(
+        res,
+        500,
+        'You have already reviewed this location.',
+      );
     }
+
     return statusHandler(res, 500, error.toString());
   }
 };
