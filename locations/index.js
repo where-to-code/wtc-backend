af5c82481c9a25cf002001c480e2d8a2f0c64e80
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-12 12:51:19 +0100

    Created update location model and endpoint

--- locations/index.js
+++ locations/index.js
@@ -10,7 +10,11 @@ router.get(
   locations.getAllLocationsCloseToUser,
 );
 router.get('/locations/:id', validate.validateId, locations.getSingleLocation);
-router.post('/locations', validate.validateLocationInput, locations.addLocation);
-
+router.post(
+  '/locations',
+  validate.validateLocationInput,
+  locations.addLocation,
+);
+router.put('/locations/:id', locations.updateLocation);
 
 module.exports = router;
