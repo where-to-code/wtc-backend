762562d8b0177400cc4274978b6fd19c7c4e1f47
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-12 12:54:08 +0100

    Added a little more breathing room

--- middleware/validations.js
+++ middleware/validations.js
@@ -20,33 +20,42 @@ const validate = (value, scheme, res, next) => {
         return statusHandler(res, 400, errMsg);
       }
       next();
-    }
+    },
   );
 };
+
 const validateQuery = (req, res, next) => {
   return validate(req.query, locationSchema.querySchema, res, next);
 };
+
 const validateId = (req, res, next) => {
   return validate(Number(req.params.id), locationSchema.queryId, res, next);
 };
+
 const validateLogin = (req, res, next) => {
   return validate(req.body, userSchema.loginSchema, res, next);
 };
+
 const validateRegister = (req, res, next) => {
   return validate(req.body, userSchema.registerSchema, res, next);
 };
+
 const validateEmail = (req, res, next) => {
   return validate(req.body.email, userSchema.email, res, next);
 };
+
 const validatePassword = (req, res, next) => {
   return validate(req.body.password, userSchema.password, res, next);
 };
+
 const validateLocationInput = (req, res, next) => {
   return validate(req.body, locationSchema.addLocationSchema, res, next);
 };
+
 const validateReviewInput = (req, res, next) => {
   return validate(req.body, reviewSchema.addReviewSchema, res, next);
 };
+
 module.exports = {
   validateQuery,
   validateId,
@@ -55,5 +64,5 @@ module.exports = {
   validateEmail,
   validatePassword,
   validateLocationInput,
-  validateReviewInput
+  validateReviewInput,
 };
