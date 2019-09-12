70a778668f3d5e01f8974ffcb724a11d8100f427
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-12 13:09:21 +0100

    Validated the endpoint

--- reviews/index.js
+++ reviews/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const reviews = require('./reviewController');
 const validate = require('../middleware/validations');
-
+const authenticate = require('../middleware/authentication');
 const router = express.Router();
 
 router.post(
   '/locations/:id/review',
+  authenticate,
   validate.validateId,
   validate.validateReviewInput,
   reviews.addReview,
