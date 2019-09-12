e647059bf10734e20b6ecb03f3762bb9915ae715
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-09-12 10:55:42 +0100

    add middleware tom protected endpoints

--- locations/index.js
+++ locations/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const locations = require('./locationController');
 const validate = require('../middleware/validations');
+const verifyToken = require('../middleware/authentication');
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.get(
   locations.getAllLocationsCloseToUser,
 );
 router.get('/locations/:id', validate.validateId, locations.getSingleLocation);
-router.post('/locations', validate.validateLocationInput, locations.addLocation);
+router.post('/locations', verifyToken, validate.validateLocationInput, locations.addLocation);
 
 
 module.exports = router;
