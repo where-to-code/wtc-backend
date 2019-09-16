e1f1c5486d9926034ddea6ba7339f48464600629
basilcea <ogbonna.basil3@gmmail.com>
2019-09-16 11:54:37 +0100

    remove repated tests

--- reviews/reviewController.spec.js
+++ reviews/reviewController.spec.js
@@ -42,46 +42,9 @@ describe('/locations/:id/review [POST]', () => {
     const res = await request(server)
       .post('/api/locations/30000/review')
       .send(review)
-      .set('Cookie',cookie)
+      .set('Cookie', cookie)
       .expect('Content-Type', /json/);
-    expect(res.body).toEqual({
-      status: 404,
-      message: 'This location does not exist'
-    });
-  });
-  it('should fail if network error', async () => {
-    const reviewErr = {
-      quietness: 2,
-      wifi_speed: 3,
-      community: 4,
-      accessibility: 5,
-      description: 'Place was great',
-      user_id: 50000
-    };
-    try {
-      await request(server)
-        .post('/api/locations/1/review')
-        .set('Cookie',cookie)
-        .send(reviewErr);
-    } catch (error) {
-      expect(error.status).toEqual(500);
-    }
-  });
-
-  it('should return an object with valid inputs', async () => {
-    const res = await request(server)
-      .post('/api/locations/300/review')
-      .send(review)
-      .set('Cookie',cookie)
-      .expect('Content-Type', /json/);
-    expect(res.status).toEqual(201);
-  });
-
-  it('should return an error if location id is not available', async () => {
-    const res = await request(server)
-      .post('/api/locations/30000/review')
-      .send(review)
-      .expect('Content-Type', /json/);
+      console.log(cookie)
     expect(res.body).toEqual({
       status: 404,
       message: 'This location does not exist'
